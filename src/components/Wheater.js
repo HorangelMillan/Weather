8d31b1759612bcfe68109efdce46818ef3730587
Horangel Millan <horangelmillan@outlook.com>
2022-05-09 23:38:58 -0500

    Mejorado la transicion del loader

--- src/components/Wheater.js
+++ src/components/Wheater.js
@@ -15,26 +15,26 @@ const Wheater = (/* {weatherD} */) => {
         <div /* style={{display: weatherD}} */>
             {
                 dateWeather != null ? <div className='weather'>{
-                    wallpaperC != null ? <img className="wallpaper-condition" src={wallpaperC} alt="" /> : null
+                    wallpaperC != null ? <img className="wallpaper-condition fadeIn" src={wallpaperC} alt="" /> : null
                 }
-                    <header>
+                    <header className="fadeIn">
                         <h1>Weather App</h1>
                         <h2>{dateWeather.name}, {dateWeather.sys?.country}</h2>
                     </header>
-                    <section>
+                    <section className="fadeIn">
                         <div><img src={conditionWeather?.icon != null ? `http://openweathermap.org/img/wn/${conditionWeather?.icon}@2x.png` : ''} alt="" className="icon" /></div>
                         <div className='info'>
                             <ul>
                                 <h3>{conditionWeather?.description}</h3>
                                 <li><i className="fa-solid fa-wind" /> <b>Wind Speed: </b>{dateWeather.wind?.speed} m/s</li>
-                                <li><i className="fa-solid fa-cloud" /> <b>Clouds: </b>{dateWeather.clouds?.all}%</li>
+                                <li><i className="fa-solid fa-cloud " /> <b>Clouds: </b>{dateWeather.clouds?.all}%</li>
                                 <li><b>Pressure: </b>{dateWeather.main?.pressure} m.s.n.m.</li>
                                 <li><b>Humidity: </b>{dateWeather.main?.humidity}%</li>
                             </ul>
                         </div>
                         <h3 className='degrees'><i className="fa-solid fa-temperature-half" /> {degree ? `${Math.floor((dateWeather.main?.temp - 273.15) * 9 / 5 + 32)}F°` : `${Math.floor(dateWeather.main?.temp - 273.15)}C°`}</h3>
                     </section>
-                    <div>
+                    <div className="fadeIn">
                         <button onClick={switchDegree}>Switch F°/C°</button>
                     </div></div> : <div className='lodader'>
                     <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
